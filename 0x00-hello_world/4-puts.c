64552615d3390b72014b572c844f92bdc10d44a3
oxygengas02 <oxygengas02@yahoo.com>
2022-09-08 07:28:06 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,11 @@
 
 /**
  * main - prints the phrase "Programming is like building a multilingual puzzle
- * 
+ *
  * return: 0 after printing the function
  */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-
 return (0);
 }
