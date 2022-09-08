53e4596a8a83d2284dd75dcf481a1a5e883c7792
oxygengas02 <oxygengas02@yahoo.com>
2022-09-08 07:24:17 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - prints the phrase "Programming is like building a multilingual puzzle"
- *
+ * main - prints the phrase "Programming is like building a multilingual puzzle
+ * 
  * return: 0 after printing the function
  */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
+
 return (0);
 }
