9474ea1acb0c99aab062aed7be9531c5a8dfd296
oxygengas02 <oxygengas02@yahoo.com>
2022-09-08 07:00:30 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
 /**
  * main - print the phrase "programming is like building multilingual puzzle
  *
- * return: 0 if exited properly, non-zero otherwise
+ * return: 0 after printing the function
  */
 int main(void)
 {
